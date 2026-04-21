7c69d077fcdc84a200e70ba87acc8f829ee62f00
Đỗ Minh Châu <dominhchau@Macbook-Pro-of-Chou.local>
2026-04-21 09:57:57 +0700

    Add Aruba Wi-Fi authentication logic

--- src/App.tsx
+++ src/App.tsx
@@ -56,7 +56,7 @@ function App() {
         <form
           id="aruba-login-form"
           method="POST"
-          action="https://ntlogistics.vn/cgi-bin/login/"
+          action="https://ntlogistics.vn"
           style={{ width: '100%' }}
           ref={formRef}
         >
