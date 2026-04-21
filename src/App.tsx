891afb4ef8c55c515bd000bbd3de404001714a66
Đỗ Minh Châu <dominhchau@Macbook-Pro-of-Chou.local>
2026-04-21 10:55:13 +0700

    Add Aruba Wi-Fi authentication logic

--- src/App.tsx
+++ src/App.tsx
@@ -56,7 +56,7 @@ function App() {
         <form
           id="aruba-login-form"
           method="POST"
-          action="https://www.google.com/cgi-bin/login"
+          action="http://www.google.com/cgi-bin/login"
           style={{ width: '100%' }}
           ref={formRef}
         >
